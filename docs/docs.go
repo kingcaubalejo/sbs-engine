ad77049bddd5f1bfe7f8fc2968aecb9b765fee8a
King Lawrence Caubalejo <kinglawrencecaubalejo@Lawrence.local>
2026-03-23 13:19:24 +0800

    feat(docs): Update Swagger file

--- docs/docs.go
+++ docs/docs.go
@@ -757,7 +757,7 @@ var SwaggerInfo = &swag.Spec{
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "SBS Engine API",
-	Description:      "REST API for the SBS (Sermon By Sermon) Engine service.",
+	Description:      "REST API for the SBS (Spiritual Building Stones) Engine service.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,7 @@
 //
 //	@title			SBS Engine API
 //	@version		1.0
-//	@description	REST API for the SBS (Sermon By Sermon) Engine service.
+//	@description	REST API for the SBS (Spiritual Building Stones) Engine service.
 //	@host			localhost:8080
 //	@BasePath		/api
 package server
